5076b54f3f53976de7771c8d2adfb9445f9f4aa0
Angel Say <aas2215@columbia.edu>
2014-11-01 21:01:50 -0400

    added test program

--- test/test.c
+++ test/test.c
@@ -1,4 +1,14 @@
-int main(int argc, char **argv) 
+#include <sched.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stdio.h>
+int main(int argc, char **argv)
 {
+	int result;
+	struct sched_param param;
+	param.sched_priority = 0;
+	result = syscall(156, 1, 6, &param);
+	if(result < 0)
+		perror("error");
 	return 0;
 }
